082e601a0ef2ec7f1c4ceef3aa3613e518868dff
Vladimir Solomonov <to-rin@yandex.ru>
2022-01-16 01:24:17 +0500

    - make header. preliminary version

--- src/js/main.js
+++ src/js/main.js
@@ -10,78 +10,6 @@ const srchFormBtnElem = document.querySelector('.js-srch-area-btn');
 const menuElem = document.querySelector('.js-main-nav')
 const mobMenuBtnElem = document.querySelector('.js-nav-btn');
 
-// const bodyElem = document.querySelector('body');
-// const burgerBtnElem= document.querySelector('.js-btn-hamburger');
-// const closeMenuBtnElem= document.querySelector('.js-close-popup');
-// const headMenuElem= document.querySelector('.js-main-nav');
-// const popupWrapperElem= document.querySelector('.js-main-nav-wrapper');
-// const closeContBtnElem= document.querySelector('.js-contacts-close-btn');
-// const contElem = document.querySelector('.js-contacts');
-
-// const headerElem = document.querySelector('.js-header');
-// const logoElem = document.querySelector('.header__logo');
-// const srchFormElem = document.querySelector('.js-search-form');
-
-
-
-// const closeSrchBtnElem = document.querySelector('.js-search-form__close');
-
-// const openMobMenu = () => {
-//     headMenuElem.classList.add('popup-open');
-//     burgerBtnElem.classList.add('hamburger-active');
-//     bodyElem.style.overflow = 'hidden';
-    
-//     setTimeout( () => {
-//         window.addEventListener('click', closeMobMenu);
-//     }, 500);
-// };
-
-// const closeMobMenu = (event) => {
-//     // console.log(event.target);
-//     if (event.target === closeMenuBtnElem|| event.target === popupWrapperElem) {
-//         headMenuElem.classList.remove('popup-open');
-//         burgerBtnElem.classList.remove('hamburger-active');
-//         bodyElem.style.overflow = 'auto';
-//         window.removeEventListener('click', closeMobMenu);
-//     }
-// };
-
-// const closeContacts = () => {
-//     contElem.classList.add('contacts-hidden');
-//     window.addEventListener('scroll', checkContactsPosition);
-// };
-
-// const checkContactsPosition = () => {
-//     const checkElemPos1 = contElem.parentElement.getBoundingClientRect().top
-//     const checkElemPos2 = contElem.parentElement.getBoundingClientRect().bottom
-
-//     if (contElem.classList.contains('contacts-hidden')) {
-//         if ( window.innerHeight < checkElemPos1 || checkElemPos2 < 0 ) {
-//             contElem.classList.remove('contacts-hidden');
-//             window.removeEventListener('scroll', checkContactsPosition);
-//         }
-//     }
-// };
-
-// const openSearch = (event) => {
-//     event.preventDefault();
-
-//     if (event.target === srchBtnElem) {
-//         const contrlHeight = Math.round(parseFloat(getComputedStyle(srchFormElem.parentElement).height));
-//         // console.log(contrlHeight);
-//         logoElem.style.minHeight = contrlHeight + 'px';
-//         srchFormElem.parentElement.style.minHeight = contrlHeight + 'px';
-        
-//         headerElem.classList.add('open-search');
-//         document.querySelector('.search-form__field').focus();
-//     };
-    
-//     srchFormElem.removeEventListener('click', openSearch);
-//     setTimeout ( () => {
-//         srchFormElem.addEventListener('click', closeSearch);
-//     }, 500);
-// };
-
 const openSearchList = () => {
     srchFormElem.classList.toggle('search-area-open');
 };
@@ -91,26 +19,6 @@ const openMobMenu = () => {
     mobMenuBtnElem.classList.toggle('nav-btn-active');
 };
 
-// const closeSearch = (event) => {
-//     console.log(event.currentTarget);
-//     if (event.target === closeSrchBtnElem) {
-//         event.preventDefault();
-//         headerElem.classList.remove('open-search');
-
-//         logoElem.style.minHeight = '';
-//         srchFormElem.parentElement.style.minHeight = '';
-//     };
-//     srchFormElem.removeEventListener('click', closeSearch);
-//     setTimeout ( () => {
-//         srchFormElem.addEventListener('click', openSearch);
-//     }, 100);
-// };
-
-// burgerBtnElem.addEventListener('click', openMobMenu);
-// closeContBtnElem.addEventListener('click', closeContacts);
-
-// srchFormElem.addEventListener('click', openSearch);
 
 srchFormBtnElem.addEventListener('click', openSearchList);
-
 mobMenuBtnElem.addEventListener('click', openMobMenu);
