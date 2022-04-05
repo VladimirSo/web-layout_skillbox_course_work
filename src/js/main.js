ef5fd49ec3aafd37cdc955252c646a49476b06d4
Vladimir Solomonov <to-rin@yandex.ru>
2022-04-06 03:30:01 +0500

    - make contacts section, footer

--- src/js/main.js
+++ src/js/main.js
@@ -135,3 +135,78 @@ const swiper3 = new Swiper(swiperAdviсesElem, {
     }
   }
 });
+
+//
+const contactPhone = document.getElementById("contactPhone");
+
+const im = new Inputmask({"mask": "+7 (999) 999-9999"});
+im.mask(contactPhone);
+
+//
+const validation = new JustValidate('#contactForm',
+  {
+    errorFieldCssClass: 'is-invalid',
+    successFieldCssClass: 'is-success',
+    errorLabelCssClass: 'is-label-invalid',
+    errorLabelStyle: {
+      color: '#ff6972',
+      fontSize: '12px',
+    },
+    focusInvalidField: true,
+    lockForm: true,
+  }
+);
+
+validation
+  .addField('#contactName', [
+      {
+        rule: 'required',
+        errorMessage: 'Заполните это поле',
+      },
+      {
+        rule: 'minLength',
+        value: 2,
+        errorMessage: 'Недопустимый формат',
+      },
+      {
+        rule: 'maxLength',
+        value: 30,
+        errorMessage: 'Недопустимый формат',
+      },
+    ])
+
+  .addField('#contactEmail', [
+      {
+        rule: 'required',
+        errorMessage: 'Заполните это поле',
+      },
+      {
+        rule: 'email',
+        errorMessage: 'Недопустимый формат',
+      },
+    ])
+
+  .addField('#contactPhone', [
+      {
+        rule: 'required',
+        errorMessage: 'Заполните это поле',
+
+      },
+      {
+        rule: 'length',
+        errorMessage: 'Недопустимый формат',
+        validator: () => {
+          const phone = contactPhone.inputmask.unmaskedvalue();
+          return Number(phone) && phone.length === 10
+        },        
+      }
+    ])
+
+  .addField('#contactCheck', [
+    {
+        rule: 'required',
+        errorMessage: 'Заполните это поле',
+    },
+  ]);
+
+
